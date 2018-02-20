82b11085df2e6565f6304af065516d6b0242e2a7
Aaron Tan <cheeseisdisgusting@gmail.com>
2018-02-19 22:19:18 -0500

    Moved location of unit tests

--- src/lyric_grabber.py
+++ src/lyric_grabber.py
@@ -1,5 +1,3 @@
-import file_writer
-
 import json
 from urllib.parse import urlencode, quote_plus
 import urllib.request
@@ -17,11 +15,6 @@ except:
     from bs4 import BeautifulSoup, Comment
   except ImportError:
     raise ImportError('Can\'t find BeautifulSoup; please install it via "pip install BeautifulSoup4"')
-
-try:
-  import taglib
-except ImportError:
-  raise ImportError('Can\'t find pytaglib; please install it via "pip install pytaglib"')
 
 LYRICWIKI_URL_BASE = 'http://lyrics.wikia.com/api.php?'
 AZLYRICS_URL_BASE = 'https://search.azlyrics.com/search.php?'
@@ -174,64 +167,3 @@ def Musixmatch_get_lyrics(title):
     return False
 
   return False
-
-# tracks = [
-#   {'Artist': 'Boy',                 'Title': 'Little Numbers'},
-#   {'Artist': 'Natalia Lafourcade',  'Title': 'Tú sí sabes quererme'},
-#   {'Artist': 'Pierre Lapointe',     'Title': 'Au bar des suicidés'},
-#   {'Artist': '曲婉婷',               'Title': '我的歌声里'},
-#   {'Artist': '方皓玟',               'Title': '你是我本身的傅奇'}
-# ]
-
-# for item in tracks:
-#   artist = item['Artist']
-#   title = item['Title']
-#   lyrics = LyricWiki_get_lyrics(artist, title)
-  # if lyrics:
-  #   write_lyrics_to_txt(title + '_lyricwiki.txt', lyrics)
-  # else:
-  #   print('[INFO] No lyrics found for file: {file}'.format(file=title))
-  # lyrics = AZLyrics_get_lyrics(title)
-  # if lyrics:
-  #   write_lyrics_to_txt(title + '_azlyrics.txt', lyrics)
-  # else:
-  #   print('[INFO] No lyrics found for file: {file}'.format(file=title))
-  # lyrics = Musixmatch_get_lyrics(title)
-  # if lyrics:
-  #   write_lyrics_to_txt(title + '_musixmatch.txt', lyrics)
-  # else:
-  #   print('[INFO] No lyrics found for file: {file}'.format(file=title))
-
-# for file in os.listdir(os.curdir):
-#   if os.path.isfile(file) and (file.endswith('.mp3') or \
-#     file.endswith('.mpc') or file.endswith('.flac') or \
-#     file.endswith('.mp4') or file.endswith('.asf') or \
-#     file.endswith('.aiff') or file.endswith('.wav') or \
-#     file.endswith('.tta') or file.endswith('.wv') or \
-#     file.endswith('.ogg') or file.endswith('.oga') or \
-#     file.endswith('.spx') or file.endswith('.opus')) or \
-#     file.endswith('.m4a'):
-
-#     filename = os.getcwd() + '/' + file
-#     song = taglib.File(filename)
-
-#     try:
-#       artist = song.tags['ARTIST'][0]
-#       title = song.tags['TITLE'][0]
-#       # print(str(title) + ' ' + str(artist))
-#     except:
-#       print('[ERROR] Metadata reading error for file: {file}'.format(file=file))
-#       continue
-
-#     try:
-#       # lyrics = LyricWiki_get_lyrics(artist, title)
-#       # lyrics = AZLyrics_get_lyrics(title)
-#       lyrics = Musixmatch_get_lyrics(title)
-#       # print(lyrics)
-
-#       if lyrics:
-#         write_lyrics_to_txt(file, lyrics)
-#       else:
-#         print('[INFO] No lyrics found for file: {file}'.format(file=file))
-#     except:
-#       print('[ERROR] Something went horribly wrong!')
---
+++ src/main.py
@@ -0,0 +1,70 @@
+import file_writer
+import lyric_grabber
+
+import os
+
+try:
+  import taglib
+except ImportError:
+  raise ImportError('Can\'t find pytaglib; please install it via "pip install pytaglib"')
+
+tracks = [
+  {'Artist': 'Boy',                 'Title': 'Little Numbers'},
+  {'Artist': 'Natalia Lafourcade',  'Title': 'Tú sí sabes quererme'},
+  {'Artist': 'Pierre Lapointe',     'Title': 'Au bar des suicidés'},
+  {'Artist': '曲婉婷',               'Title': '我的歌声里'},
+  {'Artist': '方皓玟',               'Title': '你是我本身的傅奇'}
+]
+
+for item in tracks:
+  artist = item['Artist']
+  title = item['Title']
+  lyrics = lyric_grabber.LyricWiki_get_lyrics(artist, title)
+  if lyrics:
+    file_writer.write_lyrics_to_txt(title + '_lyricwiki.txt', lyrics)
+  else:
+    print('[INFO] No lyrics found for file: {file}'.format(file=title))
+  lyrics = lyric_grabber.AZLyrics_get_lyrics(title)
+  if lyrics:
+    file_writer.write_lyrics_to_txt(title + '_azlyrics.txt', lyrics)
+  else:
+    print('[INFO] No lyrics found for file: {file}'.format(file=title))
+  lyrics = lyric_grabber.Musixmatch_get_lyrics(title)
+  if lyrics:
+    file_writer.write_lyrics_to_txt(title + '_musixmatch.txt', lyrics)
+  else:
+    print('[INFO] No lyrics found for file: {file}'.format(file=title))
+
+# for file in os.listdir(os.curdir):
+#   if os.path.isfile(file) and (file.endswith('.mp3') or \
+#     file.endswith('.mpc') or file.endswith('.flac') or \
+#     file.endswith('.mp4') or file.endswith('.asf') or \
+#     file.endswith('.aiff') or file.endswith('.wav') or \
+#     file.endswith('.tta') or file.endswith('.wv') or \
+#     file.endswith('.ogg') or file.endswith('.oga') or \
+#     file.endswith('.spx') or file.endswith('.opus')) or \
+#     file.endswith('.m4a'):
+
+#     filename = os.getcwd() + '/' + file
+#     song = taglib.File(filename)
+
+#     try:
+#       artist = song.tags['ARTIST'][0]
+#       title = song.tags['TITLE'][0]
+#       # print(str(title) + ' ' + str(artist))
+#     except:
+#       print('[ERROR] Metadata reading error for file: {file}'.format(file=file))
+#       continue
+
+#     try:
+#       # lyrics = LyricWiki_get_lyrics(artist, title)
+#       # lyrics = AZLyrics_get_lyrics(title)
+#       lyrics = lyric_grabber.Musixmatch_get_lyrics(title)
+#       # print(lyrics)
+
+#       if lyrics:
+#         file_writer.write_lyrics_to_txt(file, lyrics)
+#       else:
+#         print('[INFO] No lyrics found for file: {file}'.format(file=file))
+#     except:
+#       print('[ERROR] Something went horribly wrong!')
