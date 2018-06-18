ac69e54ea5b7136fe22a73179900bee4dfb5236c
Aaron Tan <cheeseisdisgusting@gmail.com>
2018-06-17 21:54:24 -0400

    Revert font change for Macs
    - Turns out it looks really ugly. :|

--- src/gui/appearance.py
+++ src/gui/appearance.py
@@ -6,12 +6,12 @@ HIGHLIGHT_COLOUR = QtGui.QColor(186, 221, 244)
 ALTERNATE_COLOUR_ONE = QtGui.QColor(245, 245, 245)
 
 if utils.IS_MAC:
-  LARGE_FONT = QtGui.QFont('Sans Serif', 24)
-  MEDIUM_FONT = QtGui.QFont('Sans Serif', 18)
-  SMALL_FONT = QtGui.QFont('Sans Serif', 13)
-  SMALL_FONT_BOLD = QtGui.QFont('Sans Serif', 13, QtGui.QFont.Bold)
-  SMALLER_FONT = QtGui.QFont('Sans Serif', 12)
-  TINY_FONT = QtGui.QFont('Sans Serif', 10)
+  LARGE_FONT = QtGui.QFont('.SF NS Text', 24)
+  MEDIUM_FONT = QtGui.QFont('.SF NS Text', 18)
+  SMALL_FONT = QtGui.QFont('.SF NS Text', 13)
+  SMALL_FONT_BOLD = QtGui.QFont('.SF NS Text', 13, QtGui.QFont.Bold)
+  SMALLER_FONT = QtGui.QFont('.SF NS Text', 12)
+  TINY_FONT = QtGui.QFont('.SF NS Text', 10)
 elif utils.IS_WINDOWS:
   LARGE_FONT = QtGui.QFont('Sans Serif', 16)
   MEDIUM_FONT = QtGui.QFont('Sans Serif', 14)
